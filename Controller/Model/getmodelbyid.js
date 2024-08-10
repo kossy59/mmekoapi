940bc6ea89a31e5149a21290b70315008b3fc376
Nwaforchuks <chukwuman03@gmail.com>
2024-08-10 03:35:38 +0100

    model view

---
+++ Controller/Model/getmodelbyid.js
@@ -0,0 +1,61 @@
+const {connectdatabase} = require('../../config/connectDB');
+const sdk = require("node-appwrite");
+
+const createModel = async (req,res)=>{
+
+    const hostid = req.body.hostid;
+   
+   
+    if(!hostid){
+        return res.status(400).json({"ok":false,'message': 'user Id invalid!!'})
+    }
+
+    let data = await connectdatabase()
+
+    try{
+      
+           let userdb = data.databar.listDocuments(data.dataid,data.modelCol)
+           let currentuser = (await userdb).documents.find(value=>{
+            return value.$id === hostid
+           })
+
+           if(!currentuser){
+            return res.status(409).json({"ok":false,"message":`user host empty`})
+           }
+
+
+            let host =  {
+                 hostid: currentuser.$id,
+                 photolink: currentuser.photolink,
+                 verify: currentuser.verify,
+                 name : currentuser.name,
+                 age : currentuser.age,
+                 location : currentuser.location,
+                 price : currentuser.price,
+                 duration : currentuser.duration,
+                 bodytype : currentuser.bodytype,
+                 smoke: currentuser.smoke,
+                 drink : currentuser.drink,
+                 interestedin : currentuser.interestedin,
+                 height : currentuser.height,
+                 weight : currentuser.weight,
+                 description : currentuser.description,
+                 gender:currentuser.gender,
+                 timeava:currentuser.timeava,
+                 daysava : currentuser.daysava,
+                 hosttype : currentuser.hosttype,
+                 userid: currentuser.userid
+
+              }
+        
+
+
+            return res.status(200).json({"ok":true,"message":`Model Fetched successfully`,host})
+      
+          
+          }catch(err){
+           return res.status(500).json({"ok":false,'message': `${err.message}!`});
+       }
+}
+
+module.exports = createModel
