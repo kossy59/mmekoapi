e1f4f0afd37de1b7733dc633b067305773e0916e
Nwaforchuks <chukwuman03@gmail.com>
2024-08-17 06:39:31 +0100

    model verification

--- Controller/profile/getprofile.js
+++ Controller/profile/getprofile.js
@@ -7,18 +7,34 @@ const readProfile = async (req,res)=>{
    
     let data = await connectdatabase()
 
+    let ISmodel;
+
     try{
 
             let  dupplicate = await data.databar.listDocuments(data.dataid,data.colid)
+            let  model = await data.databar.listDocuments(data.dataid,data.modelCol)
 
             let du = dupplicate.documents.filter(value=>{
                 return value.$id === userid
                })
+
+               let modelava = model.documents.find(value =>{
+                return value.userid === userid;
+               })
+
+               if(modelava){
+                ISmodel = true
+               }else{
+                ISmodel = false;
+               }
         
                if(!du[0]){
                 return res.status(409).json({"ok":false,'message': 'current user can not edit this post!!'});
         
                }
+
+               du[0].model = ISmodel;
+               du[0].modelID = modelava.$id
             return res.status(200).json({"ok":true,"message":`All Post`,profile:du[0]})
       
           
