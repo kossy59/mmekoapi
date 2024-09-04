ae7c1ab7d5e41660e504448cdd7995793078ac73
Nwaforchuks <chukwuman03@gmail.com>
2024-09-04 23:15:09 +0100

    cors test

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const { setInterval } = require('timers')
 
 //https://Mmekosocial.onrender.com
 
-app.use(credentials)
-app.use(cors(corsOptions));
+// app.use(credentials)
+// app.use(cors(corsOptions));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(cookieParser());
