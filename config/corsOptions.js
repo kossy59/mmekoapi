04bb32267a3475e51028e538116d066ce7f3166d
Nwaforchuks <chukwuman03@gmail.com>
2024-09-04 22:36:33 +0100

    test message second

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,7 @@ const whiteList = require('./allowedOrigin')
 
 const corsOptions = {
     origin : (origin,callback)=>{
-        if(whiteList.indexOf(origin)!= -1 || !origin){
+        if(whiteList.indexOf(origin) !== -1 || !origin){
             callback(null,true)
         }else{
             callback(new Error('not allowed by cors'))
