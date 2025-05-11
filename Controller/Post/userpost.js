2ad4d468a189ad6f7f4307ab42efd0f4293d323d
Emmanuel Fredrick <emmanuelfredrick66@gmail.com>
2025-05-11 17:22:18 +0100

    adding ffmpeg

--- Controller/Post/userpost.js
+++ Controller/Post/userpost.js
@@ -1,148 +1,130 @@
-// const {connectdatabase} = require('../../config/connectDB');
-// const sdk = require("node-appwrite");
-const postdata = require("../../Models/post")
-const commentdata = require("../../Models/comment")
-const likedata = require("../../Models/like")
-const userdata = require("../../Models/userdb")
-const comdata = require("../../Models/usercomplete")
-const { saveFile, uploadSingleFileToCloudinary } = require("../../utiils/cloudinary")
+const fs = require("fs");
+const path = require("path");
+const ffmpeg = require("fluent-ffmpeg");
+
+const postdata = require("../../Models/post");
+const commentdata = require("../../Models/comment");
+const likedata = require("../../Models/like");
+const userdata = require("../../Models/userdb");
+const comdata = require("../../Models/usercomplete");
+
+const { uploadSingleFileToCloudinary } = require("../../utiils/cloudinary");
+
+const compressVideo = (inputPath, outputPath) => {
+    return new Promise((resolve, reject) => {
+        ffmpeg(inputPath)
+            .outputOptions([
+                "-vcodec libx264",
+                "-crf 28", // compression level
+                "-preset veryfast",
+                "-acodec aac",
+                "-b:a 128k",
+                "-movflags +faststart"
+            ])
+            .on("end", () => resolve(outputPath))
+            .on("error", (err) => reject(err))
+            .save(outputPath);
+    });
+};
 
 const createPost = async (req, res) => {
-
-    console.log("req.body.data", req.body.data);
-    const data = JSON.parse(req.body.data);
-    console.log("data", data);
-
-    const userid = data.userid;
-    let content = data.content;
-    let posttype = data.posttype;
-
-    /**
-     * This implementation uploads the file to a folder on the server
-     * and manipulates the filesystem of the server
-     */
-    // const result = await saveFile(req.file, req.file.path, `assets/${posttype}s`);
-
-    /**
-     * This implementation allows for in memory file upload manipulation
-     * This prevents accessing the filesystem of the hosted server
-     */
-    const result = await uploadSingleFileToCloudinary(req.file, `assets/${posttype}s`);
-
-    console.log("result: ", result)
-
-    const postfilelink = result.file_link
-    const postfilepublicid = result.public_id
-   
-    if (!userid) {
-        return res.status(400).json({ "ok": false, 'message': 'user Id invalid!!' })
-    }
-
-    console.log("posting image", postfilelink, postfilepublicid)
-
-    // let data = await connectdatabase()
-
     try {
-      
+        const data = JSON.parse(req.body.data);
+        const userid = data.userid;
+        let content = data.content || "";
+        let posttype = data.posttype;
 
-        // if(!postfilelink){
-        //     console.log("no photolink")
-        //     postfilelink = ""
-        // }
-
-        if (!content) {
-            content = ""
+        if (!userid) {
+            return res.status(400).json({ ok: false, message: "User ID invalid!" });
         }
-           
-        let posts = {
+
+        let result;
+
+        if (posttype === "video") {
+            // Compress video before uploading
+            const tempPath = `./temp/${Date.now()}-${req.file.originalname}`;
+            fs.writeFileSync(tempPath, req.file.buffer);
+
+            const compressedPath = tempPath.replace(path.extname(tempPath), "-compressed.mp4");
+
+            await compressVideo(tempPath, compressedPath);
+
+            result = await uploadSingleFileToCloudinary(
+                fs.createReadStream(compressedPath),
+                `assets/${posttype}s`
+            );
+
+            fs.unlinkSync(tempPath);         // Clean up original
+            fs.unlinkSync(compressedPath);   // Clean up compressed
+        } else {
+            // Upload directly for non-video types
+            result = await uploadSingleFileToCloudinary(req.file, `assets/${posttype}s`);
+        }
+
+        const postfilelink = result.file_link;
+        const postfilepublicid = result.public_id;
+
+        const posts = {
             userid,
             postfilelink,
             postfilepublicid,
             posttime: `${Date.now()}`,
             content,
-            posttype
-        }
-            
+            posttype,
+        };
 
-        // let currentpostid = await data.databar.createDocument(data.dataid,data.postCol,sdk.ID.unique(),posts)
-        await postdata.create(posts)
+        await postdata.create(posts);
 
-        let currentpostid = await postdata.findOne({ posttime: posts.posttime })
-        let postdb = await postdata.find().exec()
-        let userdb = await userdata.find().exec()
-        let comdb = await comdata.find().exec()
-
-        let likedb = await likedata.find().exec()
-        let commentdb = await commentdata.find().exec()
+        const currentpostid = await postdata.findOne({ posttime: posts.posttime });
+        const [userdb, comdb, likedb, commentdb] = await Promise.all([
+            userdata.find().exec(),
+            comdata.find().exec(),
+            likedata.find().exec(),
+            commentdata.find().exec()
+        ]);
 
         let post = {};
-        console.log("current posts id " + currentpostid._id)
 
-          
-             
-
-        for (let j = 0; j < userdb.length; j++) {
-
-            for (let k = 0; k < comdb.length; k++) {
-
-                       
-                       
-
-                if (String(currentpostid.userid) === String(userdb[j]._id) && String(comdb[k].useraccountId) === String(userdb[j]._id)) {
-
-                    console.log("found post")
+        for (let user of userdb) {
+            for (let com of comdb) {
+                if (
+                    String(currentpostid.userid) === String(user._id) &&
+                    String(com.useraccountId) === String(user._id)
+                ) {
                     post = {
-                        username: `${userdb[j].firstname} ${userdb[j].lastname}`,
-                        nickname: `${userdb[j].nickname}`,
-                        userphoto: `${comdb[k].photoLink}`,
-                        content: `${currentpostid.content}`,
-                        postphoto: `${currentpostid.postlink}`,
-                        posttime: `${currentpostid.posttime}`,
-                        posttype: `${currentpostid.posttype}`,
-                        postid: `${currentpostid._id}`,
+                        username: `${user.firstname} ${user.lastname}`,
+                        nickname: user.nickname,
+                        userphoto: com.photoLink,
+                        content: currentpostid.content,
+                        postphoto: currentpostid.postfilelink,
+                        posttime: currentpostid.posttime,
+                        posttype: currentpostid.posttype,
+                        postid: currentpostid._id,
                         like: [],
                         comment: [],
-                        userid: userdb[j]._id
-                    }
-
-                    console.log("post time why " + currentpostid.posttime)
-                    console.log("post id why " + currentpostid._id)
-                    console.log("post type why " + currentpostid.posttype)
-
-                           
-
+                        userid: user._id,
+                    };
                 }
-
             }
-
         }
 
-            
-
-            
-        for (let j = 0; j < commentdb.length; j++) {
-            if (currentpostid._id === commentdb[j].postid) {
-                post.comment.push(commentdb[j])
+        for (let comment of commentdb) {
+            if (String(currentpostid._id) === String(comment.postid)) {
+                post.comment.push(comment);
             }
-
         }
-            
 
-          
-        for (let j = 0; j < likedb.length; j++) {
-            if (currentpostid._id === likedb[j].postid) {
-                post.like.push(likedb[j])
+        for (let like of likedb) {
+            if (String(currentpostid._id) === String(like.postid)) {
+                post.like.push(like);
             }
-
         }
-        console.log("posts " + post.posttime)
 
-        return res.status(200).json({ "ok": true, "message": `Posted successfully`, post: post })
-      
-          
+        return res.status(200).json({ ok: true, message: "Posted successfully", post });
     } catch (err) {
-        return res.status(500).json({ "ok": false, 'message': `${err.message}!` });
+        console.error("Upload error:", err);
+        return res.status(500).json({ ok: false, message: err.message });
     }
-}
+};
 
-module.exports = createPost
+module.exports = createPost;
