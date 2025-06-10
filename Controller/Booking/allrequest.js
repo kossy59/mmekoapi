3b65aad415884a511b7623968bd0553ab12573ba
Aycodez <aycom595@gmail.com>
2025-06-10 14:31:43 +0100

    implemented all changes

--- Controller/Booking/allrequest.js
+++ Controller/Booking/allrequest.js
@@ -1,136 +1,141 @@
-const bookingdb = require("../../Models/book")
-const modeldb = require("../../Models/models")
-const photoLink = require("../../Models/usercomplete")
-const userdb = require("../../Models/userdb")
-const admindb = require("../../Models/admindb")
+const bookingdb = require("../../Models/book");
+const modeldb = require("../../Models/models");
+const photoLink = require("../../Models/usercomplete");
+const userdb = require("../../Models/userdb");
+const admindb = require("../../Models/admindb");
 
-const createLike = async (req,res)=>{
-    
-    const userid = req.body.userid
-    const modelid = req.body.modelid
-    
-   
-    if(!userid){
-        return res.status(400).json({"ok":false,'message': 'user Id invalid!!'})
+const createLike = async (req, res) => {
+  const userid = req.body.userid;
+  const modelid = req.body.modelid;
+
+  if (!userid) {
+    return res.status(400).json({ ok: false, message: "user Id invalid!!" });
+  }
+  console.log("untop init db");
+
+  try {
+    let users = await bookingdb.find({ userid: userid }).exec();
+    let adminmessage = await admindb.find({ userid: userid }).exec();
+
+    let model = [];
+
+    if (modelid) {
+      let mod = await bookingdb.find({ modelid: modelid }).exec();
+      model = mod
+        .filter((value) => {
+          return (
+            String(value.status) === "pending" ||
+            String(value.status) === "accepted"
+          );
+        })
+        .reverse();
     }
-    console.log('untop init db')
 
-   
+    let user = users.filter((value) => {
+      return (
+        String(value.status) === "accepted" ||
+        String(value.status) === "decline" ||
+        String(value.status) === "pending"
+      );
+    });
 
+    //console.log('under user pending')
 
-    //let data = await connectdatabase()
+    let approve = [];
+    const now = new Date();
+    const thirtyDaysAgo = new Date(now.getTime() - 30 * 24 * 60 * 60 * 1000);
 
-    try{
-         const users = await bookingdb.find({userid:userid}).exec()
-         const adminmessage = await admindb.find({userid:userid}).exec()
+    // Filter model array for bookings created within the last 30 days
+    model = model.filter((m) => {
+      const created = new Date(m.createdAt);
+      return created >= thirtyDaysAgo && created <= now;
+    });
 
-         let model = [];
+    adminmessage = adminmessage
+      .filter((m) => {
+        const created = new Date(m.createdAt);
+        return created >= thirtyDaysAgo && created <= now;
+      })
+      .reverse();
+    user = user.filter((m) => {
+      const created = new Date(m.createdAt);
+      return created >= thirtyDaysAgo && created <= now;
+    });
 
-         if(modelid){
-            
-             let mod = await bookingdb.find({modelid : modelid}).exec()
-             model = mod.filter(value =>{
-              
-                return String(value.status) === "pending" || String(value.status) === "accepted"
-            })
+    for (let i = 0; i < model.length; i++) {
+      console.log("inside my model" + i);
+      let username = await userdb.findOne({ _id: model[i].userid }).exec();
+      let image1 = await photoLink
+        .findOne({ useraccountId: model[i].userid })
+        .exec();
+      if (username) {
+        approve.push({
+          photolink: image1.photoLink,
+          name: username.firstname,
+          status: model[i].status,
+          type: model[i].type,
+          date: model[i].date,
+          time: model[i].time,
+          modelid: model[i].modelid,
+          id: model[i]._id,
+          place: model[i].place,
+          clientid: model[i].userid,
+          createdAt: model[i].createdAt,
+          updatedAt: model[i].updatedAt,
+        });
+      }
+    }
 
-    
-         }
+    for (let i = 0; i < user.length; i++) {
+      let image = await modeldb.findOne({ _id: user[i].modelid }).exec();
 
-         let user = users.filter(value =>{
-            return String(value.status) === "accepted"  || String(value.status) === "decline"  || String(value.status) === "pending"
-         })
+      if (image) {
+        let photo = image.photolink.split(",");
 
-         
+        approve.push({
+          photolink: photo[0],
+          name: image.name,
+          status: user[i].status,
+          type: user[i].type,
+          date: user[i].date,
+          time: user[i].time,
+          modelid: user[i].modelid,
+          id: user[i]._id,
+          modeluserid: image.userid,
+          amount: image.price,
+        });
+      }
+    }
 
-          //console.log('under user pending')
+    adminmessage.forEach((value) => {
+      if (value.seen) {
+        let data = {
+          message: value.message,
+          time: `${value._id.getTimestamp().getTime()}`,
+          ismessage: true,
+          id: value._id,
+          admindb: true,
+          notification: false,
+        };
 
-          let approve = []
+        approve.push(data);
+      }
+    });
 
-           for(let i = 0; i < model.length; i++){
+    if (!approve[0]) {
+      return res.status(200).json({
+        ok: false,
+        message: "you have 0 approved request!!",
+        approve: [],
+      });
+    }
 
-            console.log("inside my model")
-            let username = await userdb.findOne({_id:model[i].userid}).exec()
-            let image1 = await photoLink.findOne({useraccountId:model[i].userid}).exec()
-            if (username){
-                
+    // approve.reverse()
+    return res.status(200).json({ ok: true, message: ` Success`, approve });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ ok: false, message: `${err.message}!` });
+  }
+};
 
-                approve.push({
-                    photolink : image1.photoLink,
-                    name : username.firstname,
-                    status : model[i].status,
-                    type : model[i].type,
-                    date : model[i].date,
-                    time : model[i].time,
-                    modelid : model[i].modelid,
-                    id : model[i]._id,
-                    place: model[i].place,
-                    clientid: model[i].userid
-                  
-
-                })
-            }
-         }
-
-         
-
-
-
-        
-
-         for(let i = 0; i < user.length; i++){
-            let image = await modeldb.findOne({_id:user[i].modelid}).exec()
-            
-            if (image){
-                let photo = image.photolink.split(",")
-
-                approve.push({
-                    photolink : photo[0],
-                    name : image.name,
-                    status : user[i].status,
-                    type : user[i].type,
-                    date : user[i].date,
-                    time : user[i].time,
-                    modelid : user[i].modelid,
-                    id : user[i]._id,
-                    modeluserid : image.userid,
-                    amount:image.price
-
-                })
-            }
-         }
-
-            adminmessage.forEach(value=>{
-
-                if(value.seen){
-
-                    let data = {
-                    message : value.message,
-                    time : `${value._id.getTimestamp().getTime()}`,
-                    ismessage:true,
-                    id:value._id,
-                    admindb:true,
-                    notification:false
-
-                    }
-
-                    approve.push(data)
-                }
-            
-            })
-
-                  if(!approve[0]) {
-                     return res.status(200).json({"ok":false,'message': 'you have 0 approved request!!',approve:[]})
-                }
-
-
-                approve.reverse()
-            return res.status(200).json({"ok":true,"message":` Success`,approve})
-      
-          
-       }catch(err){
-           return res.status(500).json({"ok":false,'message': `${err.message}!`});
-       }
-}
-
-module.exports = createLike
+module.exports = createLike;
--- Models/comment.js
+++ Models/comment.js
@@ -1,36 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Scheme = mongoose.Schema;
 
-const markertdata = new Scheme({
-
-     userid:{
-        type:String,
-        required : true
+const markertdata = new Scheme(
+  {
+    userid: {
+      type: String,
+      required: true,
     },
 
-     content:{
-        type:String,
-        required : false
+    content: {
+      type: String,
+      required: false,
     },
 
-     sharedid:{
-        type:String,
-        required : false
+    sharedid: {
+      type: String,
+      required: false,
     },
 
-     postid:{
-        type:String,
-        required : true
+    postid: {
+      type: String,
+      required: true,
     },
 
-    commenttime:{
-        type:String,
-        required : true
+    commenttime: {
+      type: String,
+      required: true,
     },
+  },
+  { timestamps: true }
+);
 
-   
-  
-  
-})
-
-module.exports = mongoose.model('Comment',markertdata);
+module.exports = mongoose.model("Comment", markertdata);
