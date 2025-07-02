c5de3410593d4a923a2520c7b38fbc9e6c84d866
Aycodez <aycom595@gmail.com>
2025-07-02 18:39:01 +0100

    private coin fix

--- Controller/Follower/delete_follower.js
+++ Controller/Follower/delete_follower.js
@@ -1,57 +1,50 @@
-const followerdb = require("../../Models/followers")
-const userdb = require("../../Models/userdb")
-const admindb = require("../../Models/admindb")
-let sendEmail = require("../../utiils/sendEmailnot")
-let sendpushnote = require("../../utiils/sendPushnot")
+const followerdb = require("../../Models/followers");
+const userdb = require("../../Models/userdb");
+const admindb = require("../../Models/admindb");
+let sendEmail = require("../../utiils/sendEmailnot");
+let sendpushnote = require("../../utiils/sendPushnot");
 
-const createModel = async (req,res)=>{
+const createModel = async (req, res) => {
+  const followerid = req.body.followerid;
+  const userid = req.body.userid;
 
-    const followerid = req.body.followerid;
-    const userid = req.body.userid
+  if (!followerid && !userid) {
+    return res.status(400).json({ ok: false, message: "user Id invalid!!" });
+  }
 
-   
-    if(!followerid && !userid ){
-        return res.status(400).json({"ok":false,'message': 'user Id invalid!!'})
+  let followed = await followerdb.find({ userid: userid }).exec();
+
+  let Isfollowed = followed.find((value) => value.followerid === followerid);
+
+  if (!Isfollowed) {
+    return res.status(400).json({ ok: false, message: "follow user first!!" });
+  }
+
+  try {
+    let client = await userdb.findOne({ _id: followerid }).exec();
+
+    if (!client) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "this user is not avalable!!" });
     }
 
-    let followed = await followerdb.find({userid:userid}).exec()
+    let respond = {
+      userid: userid,
+      message: `${client.firstname} ${client.lastname} unfollowed you`,
+      seen: true,
+    };
 
-    let Isfollowed = followed.find(value=> value.followerid === followerid)
+    await admindb.create(respond);
 
-    if(!Isfollowed ){
-        return res.status(400).json({"ok":false,'message': 'follow user first!!'})
-    }
+    await followerdb.deleteOne({ _id: Isfollowed._id });
+    await sendEmail(userid, "user unfollow you");
+    await sendpushnote(userid, "user unfollow you", "modelicon");
 
-    try{
+    return res.status(200).json({ ok: true, message: `followed successfully` });
+  } catch (err) {
+    return res.status(500).json({ ok: false, message: `${err.message}!` });
+  }
+};
 
-        let client = await userdb.findOne({_id:followerid}).exec()
-      
-        if(!client ){
-            return res.status(400).json({"ok":false,'message': 'this user is not avalable!!'})
-        }
-
-       
-        let respond = {
-            userid:userid,
-            message:`${client.firstname} ${client.lastname} unfollowed you`,
-            seen:true
-        }
-
-        await admindb.create(respond)
-
-
-        await followerdb.deleteOne({_id:Isfollowed._id})
-        await sendEmail(userid, "user unfollow you")
-        await sendpushnote(userid,"user unfollow you","modelicon")
-
-
-
-        return res.status(200).json({"ok":true,"message":`followed successfully`})
-      
-          
-    }catch(err){
-           return res.status(500).json({"ok":false,'message': `${err.message}!`});
-    }
-}
-
-module.exports = createModel
+module.exports = createModel;
--- utiils/deductPVC.js
+++ utiils/deductPVC.js
@@ -1,40 +1,33 @@
-const modeldb = require("../Models/models")
-let bookdb = require("../Models/book")
-let userdb = require("../Models/userdb")
-historydb = require("../Models/mainbalance")
-const pay = async(userid,toid,balance,amount)=>{
+const modeldb = require("../Models/models");
+let bookdb = require("../Models/book");
+let userdb = require("../Models/userdb");
+historydb = require("../Models/mainbalance");
+const pay = async (userid, toid, balance, amount) => {
+  let modelid = await modeldb.findOne({ userid: toid }).exec();
 
-    let modelid = await modeldb.findOne({userid:toid}).exec()
+  if (modelid) {
+    // getting model for knowing it booking price
+    //let model = await modeldb.findOne({_id:usmodelid}).exec()
 
-    if(modelid){
-       
+    //console.log("model price "+price)
+    let user_paying = await userdb.findOne({ _id: userid }).exec();
 
-          // getting model for knowing it booking price
-          //let model = await modeldb.findOne({_id:usmodelid}).exec()
-          
-          //console.log("model price "+price)
-         let user_paying = await userdb.findOne({_id:userid}).exec()
-        
+    let clienthistory = {
+      userid: userid,
+      details: `private call payment from ${modelid.name}`,
+      spent: `${amount}`,
+      income: "0",
+      date: `${Date.now().toString()}`,
+    };
+    modelid.earnings = (modelid?.earnings ?? 0) + Number(amount);
+    await modelid.save();
 
+    user_paying.balance = `${balance}`;
 
-          let clienthistory = {
-             userid:userid,
-             details: `private call payment from ${modelid.name}`,
-             spent: `${amount}`,
-             income: "0",
-             date: `${Date.now().toString()}`
-          }
+    await user_paying.save();
 
-
-         user_paying.balance = `${balance}`
-
-        await  user_paying.save()
-
-        await historydb.create(clienthistory)
-
-
-    }
-  
-}
+    await historydb.create(clienthistory);
+  }
+};
 
 module.exports = pay;
