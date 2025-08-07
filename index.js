b60754ba608a59d5482bfd8db68b63cc310ec69a
Mintlord3ttah <lordmint7@gmail.com>
2025-08-07 12:01:11 +0100

    CORS: reset headers

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://mmekowebsite.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
