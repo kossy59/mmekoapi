517b2db546a9fe3f8b30bd0bd0b0e671be48beaa
Mintlord3ttah <lordmint7@gmail.com>
2025-08-07 14:00:46 +0100

    CONFIG CI/CD: removed manual cors headset

--- index.js
+++ index.js
@@ -45,12 +45,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://mmekowebsite.onrender.com");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
