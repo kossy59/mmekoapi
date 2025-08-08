763ea32c9efacd8e1a02d02c22b2e341b064d042
Mintlord3ttah <lordmint7@gmail.com>
2025-08-08 15:08:44 +0100

    ACCESS AND CROSS SITE RESTRICTION BRIDGED

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ app.use(cors({
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://mmekowebsite.onrender.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 app.use(express.json());
