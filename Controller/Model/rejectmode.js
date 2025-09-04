d7086fd49d019adafa0cddb4d47ec2b0494b97f3
Chia Ernest <iternenge469@gmail.com>
2025-09-04 14:36:30 +0100

    prg

--- Controller/Model/rejectmode.js
+++ Controller/Model/rejectmode.js
@@ -1,34 +1,38 @@
 let admindb = require("../../Models/admindb");
 const models = require("../../Models/models");
+const userdb = require("../../Models/userdb");
 
-const createModel = async (req,res)=>{
+const createModel = async (req, res) => {
 
   const userid = req.body.userid;
   const docID = req.body.docid
-    
-   
-  if(!userid && !docID){
-    return res.status(400).json({"ok":false,'message': 'user Id invalid!!'})
-}
- 
-    try{
-      
-      await models.deleteOne({_id:req.body.id}).exec()
 
-      let respond = {
-        userid:userid,
-        message:`you exclusive application has been rejected`,
-        seen:true
+
+  if (!userid && !docID) {
+    return res.status(400).json({ "ok": false, 'message': 'user Id invalid!!' })
+  }
+
+  try {
+
+    await models.deleteOne({ _id: req.body.id }).exec()
+
+    let respond = {
+      userid: userid,
+      message: `you exclusive application has been rejected`,
+      seen: true
     }
-
+    const user = userdb.findOne({ "_id": userid })
+    user.modelId = "";
+    user.isModel = false
+    await user.save()
     await admindb.create(respond)
-         
-    return res.status(200).json({"ok":true,"message":`host Updated successfully`,hostid:docID})
-      
-          
-     }catch(err){
-           return res.status(500).json({"ok":false,'message': `${err.message}!`});
-       }
+
+    return res.status(200).json({ "ok": true, "message": `host Updated successfully`, hostid: docID })
+
+
+  } catch (err) {
+    return res.status(500).json({ "ok": false, 'message': `${err.message}!` });
+  }
 }
 
 module.exports = createModel
